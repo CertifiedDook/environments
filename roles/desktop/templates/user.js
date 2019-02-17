d9fa65b3343657d7dd2a53814f94a60579fd1ecf
Paweł Jan Czochański <czochanski@gmail.com>
2019-02-17 21:03:27 +0100

    Add Firefox preferences copying

---
+++ roles/desktop/templates/user.js
@@ -0,0 +1,5 @@
+user_pref("browser.sessionstore.interval", 1800000);
+user_pref("browser.cache.disk.enable", false);
+user_pref("browser.cache.memory.capacity", -1);
+user_pref("browser.cache.memory.enable", true);
+user_pref("security.webauth.u2f", true);
