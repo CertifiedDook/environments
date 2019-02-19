1090cee3d6fcc9ee77d654df0b26d01fd50a79ec
Paweł Jan Czochański <czochanski@gmail.com>
2019-02-19 18:27:39 +0100

    Add more Firefox options

--- roles/desktop/templates/user.js
+++ roles/desktop/templates/user.js
@@ -3,3 +3,5 @@ user_pref("browser.cache.disk.enable", false);
 user_pref("browser.cache.memory.capacity", -1);
 user_pref("browser.cache.memory.enable", true);
 user_pref("security.webauth.u2f", true);
+user_pref("extensions.pocket.enabled", false);
+user_pref("dom.webnotifications.enabled", false);
