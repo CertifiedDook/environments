73ac5e61e87d86e05bd8250c09a46f9fb612b015
Paweł Czochański <pawel@czochanski.pl>
2022-03-07 09:51:42 +0100

    Update user.js

--- roles/desktop/templates/user.js
+++ roles/desktop/templates/user.js
@@ -4,3 +4,4 @@ user_pref("browser.cache.memory.capacity", -1);
 user_pref("browser.cache.memory.enable", true);
 user_pref("security.webauth.u2f", true);
 user_pref("extensions.pocket.enabled", false);
+lockPref("signon.rememberSignons", false);
