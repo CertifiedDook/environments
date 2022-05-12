1683866bdd1045b164ae3a91ac28b18ad982f55e
Paweł Jan Czochański <pawel@czochanski.pl>
2022-05-12 18:05:49 +0200

    Fix for Fedora 36

--- install.sh
+++ install.sh
@@ -5,18 +5,3 @@ trap "exit" INT
 sudo dnf install ansible -y -q
 
 ansible-playbook setup.yml --tags desktop -K -i hosts.yml
-
-if test -f ~/.mozilla/native-messaging-hosts/com.justwatch.gopass.json; then
-  echo "gopass already installed for firefox"
-else
-  echo "configuring gopass firefox plugin"
-  gopass jsonapi configure --browser firefox
-  xdg-open https://addons.mozilla.org/pl/firefox/addon/gopass-bridge/
-fi
-
-if test -d ~/.password-store; then
-  echo "password-store already cloned"
-else
-  echo "cloning password-store"
-  git clone git@bitbucket.org:czocher/pass.git ~/.password-store
-fi
