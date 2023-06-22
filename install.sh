277a8841dc201116215e3e925b08a7c32998bc1e
Paweł Jan Czochański <pawel@czochanski.pl>
2023-06-22 09:00:43 +0200

    Support lockscreen unlock with Yubikey

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ trap "exit" INT
 
 if ! command -v ansible &> /dev/null
 then
-  sudo rpm-ostree install ansible --idempotent --apply-live
+  sudo rpm-ostree install ansible pamu2fcfg --idempotent --apply-live
 fi
 
 ansible-playbook setup.yml --tags desktop -K -i hosts.yml
@@ -12,4 +12,9 @@ ansible-playbook setup.yml --tags desktop -K -i hosts.yml
 if [ ! -f ~/.backup_password.asc ]; then
   PASSWORD=$(kdialog --password 'Provide the backup password')
   echo "$PASSWORD" | gpg --quiet --encrypt > ~/.backup_password.asc
+fi
+
+if [ ! -f ~/.config/Yubico/u2f_keys ]; then
+  echo 'Enter Yubikey PIN then press it...'
+  pamu2fcfg -uczocher > ~/.config/Yubico/u2f_keys
 fi
